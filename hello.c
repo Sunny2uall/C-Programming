b963c69199eaac800cb0b6f10800f6c9204e3f5f
Sunny <sourav@satpathy.com>
2025-11-01 23:20:13 +0530

    2nd update

--- hello.c
+++ hello.c
@@ -5,5 +5,5 @@ int main()
 	printf("Hello World...!!!\n");
 	printf("Welcome to C Programming...!!!\n");
 	printf("This is a test...!!!!!\n");
-	printf("And Welcome to GIT.!\n");
+	printf("And Welcome to GIT..!!\n");
 }
